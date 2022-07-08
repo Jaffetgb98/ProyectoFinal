728217d986b96e4d6affcd4fa09520f441f64b7d
Jaffetgb98 <jaffetg04@gmail.com>
2022-07-07 23:23:23 -0600

    navbar

--- videojuegos/src/app/app.module.ts
+++ videojuegos/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PrincipalComponent } from './principal/principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
