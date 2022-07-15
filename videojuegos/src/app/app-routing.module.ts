1474c5c8edd7ced29ac78db3d914572705783791
Raymond Jaffet Grijalba Badilla <jaffetg04@gmail.com>
2022-07-14 23:15:46 -0600

    style componente info

--- videojuegos/src/app/app-routing.module.ts
+++ videojuegos/src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { Enlace1Component } from './enlace1/enlace1.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PrincipalComponent } from './principal/principal.component';
 import { TarjetasComponent } from './tarjetas/tarjetas.component';
+import { InfoGameComponent } from './info-game/info-game.component';
 
 const routes: Routes = [
   {path:"link1", component:Enlace1Component},
   {path:"nav", component:NavbarComponent},
   {path:"search/:id", component:BusquedaComponent},
+  {path:"info", component:InfoGameComponent},
   {path:"principal", component:PrincipalComponent},
   {path:"", component:PrincipalComponent}
 ];
